3b4a4d44eba176740fc9d7ec056c4e37de13770e
kaykusi <80196267+kaykusi@users.noreply.github.com>
2022-12-11 20:57:58 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contacts', function () {
+    return '<h1>All Contacts</h1>';
+});
+
+Route::get('/contacts/create', function () {
+    return "<h1>Contacts Create</h1>";
+});
+
+Route::get('/contacts/{id}', function ($id) {
+    return "Contacts " . $id;
+});
+
+Route::get('/companies/{name?}', function ($name = null) {
+    if ($name) {
+        return "Company " . $name;
+    } else {
+        return "All Companies";
+    }
+});
